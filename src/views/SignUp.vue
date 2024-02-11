7bd9168f9690f6690513a36aa516d95954622659
Oleg Sergeev <o.v.sergeyev@gmail.com>
2024-02-12 00:17:48 +0300

    Форма signup

---
+++ src/views/SignUp.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+
+const email = ref()
+const password = ref()
+
+const signup = async () => {
+  console.log('test')
+}
+</script>
+
+<template>
+  <h2>Sign Up</h2>
+  <form class="flex flex-column gap-3">
+    <div class="p-inputgroup flex-1">
+      <span class="p-inputgroup-addon">
+        <i class="pi pi-user"></i>
+      </span>
+      <InputText v-model="email" type="email" placeholder="Your Email" />
+    </div>
+    <div class="p-inputgroup flex-1">
+      <span class="p-inputgroup-addon">
+        <i class="pi pi-at"></i>
+      </span>
+      <InputText v-model="password" type="password" placeholder="Password" />
+    </div>
+    <div class="flex flex-column gap-3">
+      <Button @click="signup()" label="Signup" />
+      <span>Are you already registered? <router-link to="/signin">Sign in</router-link></span>
+    </div>
+  </form>
+</template>
